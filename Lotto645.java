9b9295b317bb8239dcf283f1204b15b06ca7341a
ki-yung Kim <84295788+ki-yungkim@users.noreply.github.com>
2021-05-24 22:32:36 +0900

    Add files via upload

---
+++ Lotto645.java
@@ -0,0 +1,129 @@
+package com.work.view;
+
+//import java.util.Arrays;
+import java.util.Random;
+
+public class Lotto645 {
+
+	/*
+	 * # 개선사항 
+	 * 1. 정렬 알고리즘 O
+	 * 2. 1게임, 5게임 허용 O
+
+	 * # 기술 
+	 * -- 반복문 
+	 * => break;    역할 찾아보기
+	 * => continue; 역할 찾아보기
+	 * -- 중첩반복문 
+	 * => 반복문에 레이블 지정 방법 outerLoop: 처럼
+	 * => break 레이블명;
+	 * => continue 레이블명;
+	 * 
+	 * .Arrays 안 쓰고 정렬 해보기
+	 */
+
+	public static void main(String[] args) {
+		game1();
+		game5();
+	}
+
+	public static void game5() {
+		int[][] Numbers = new int[5][6];
+
+		for (int index = 0; index < 5; index++) {
+			for (int i = 0; i < 6; i++) {
+				Numbers[index][i] = getLottoNo();
+				for (int j = 0; j < i; j++) {
+					if (Numbers[index][i] == Numbers[index][j]) {
+						i--;
+					}
+				}
+			}
+		}
+		//int[] Sorting = new int[6];
+		
+		System.out.println("[로또 5게임]");
+		for (int index = 0; index < 5; index++) {
+			for (int j = 0; j < 6; j++) {
+				//System.out.print(Numbers[index][j] + "\t");
+			}
+			//System.out.println();
+		}
+		for (int i = 0; i < 5; i++) {    
+			for (int k = 0; k < 6; k++) { 
+				for (int j = 0; j < 5; j++) {
+					if (Numbers[i][j] > Numbers[i][(j + 1)]){
+						int tmp = Numbers[i][j];
+						Numbers[i][j] = Numbers[i][(j + 1)];
+						Numbers[i][(j + 1)] = tmp;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 6; j++) {
+				System.out.print(Numbers[i][j] + "\t");
+			}
+			System.out.println();		
+		}
+		
+		
+/*		Array.sort 쓴 경우, 안 쓰고 정렬해보기 
+		for (int j = 0; j < 5; j++) {
+			for (int i = 0; i < 6; i++) {
+				Sorting[i] = Numbers[j][i];
+			}
+			Arrays.sort(Sorting);
+			for (int k = 0; k < 6; k++) {
+				System.out.print(Sorting[k] + "\t");
+			}
+			System.out.println();		
+		}
+*/
+		}
+
+	public static void game1() {
+		int[] lottos = new int[6];
+		int uniqueLength = 0;
+
+		int randomNo;
+
+		//중복 체크
+		outerLoop:
+			while(uniqueLength < 6) {
+				randomNo = getLottoNo();
+
+				for (int index = 0; index < uniqueLength; index++) {
+					if (lottos[index] == randomNo) {
+						continue outerLoop;
+					}
+				}
+
+				lottos[uniqueLength++] = randomNo;
+			}
+		
+		for (int j = 0; j < 6 ; j++) {    
+				for (int i = 0; i < 5; i++) {
+					if (lottos[i] > lottos[(i + 1)]){
+						int tmp = lottos[i];
+						lottos[i] = lottos[(i + 1)];
+						lottos[(i + 1)] = tmp;
+					}
+				}
+			
+		}
+
+		System.out.println("[1등 당첨예약 번호] ");
+		for (int index = 0; index < lottos.length; index++) {
+			System.out.print(lottos[index] + "\t");
+		}
+		System.out.println();
+		System.out.println();
+	}
+
+	public static int getLottoNo() {
+		Random random = new Random((long)(Math.random() * System.nanoTime()));
+		return random.nextInt(45) + 1;
+	}
+
+}
---
+++ RandomTeamTest.java
@@ -0,0 +1,122 @@
+/*
+ * 6번 줄       ++ 추가 중복체크 안되던 것 수정
+ * 77 ~ 99번 줄 가나다라 순으로 정렬 추가
+ */
+
+package com.work.view;
+
+import java.util.Random;
+
+public class RandomTeamTest {
+	public static void main(String[] args) {
+		int teamMemberLengthCols = 4;
+		if (args.length == 1) {
+			teamMemberLengthCols = Integer.parseInt(args[0]);
+		} else {
+			System.out.println("[사용방법] java RandomTeamTest 팀별인원숫자");
+			System.out.println("[팀당 인원수를 입력하지 않아 팀당 인원수를 기본 4명으로 진행합니다.]");
+		}
+
+		String[] names = {
+				"김기영", "김나연", "김대현", "김수정", "김의현", "김재현", "김종환", "김주리", "김주엽", "김지원", 
+				"김현수", "김현진", "김혜진", "노한나", "문수인", "박민재", "박영규", "박유정", "신지수", "유건", 
+				"유준성", "이태규", "이택근", "이희원", "장현우", "조성현", "최낙원", "한다예", "황윤아", "유동국"
+		};
+
+		String[][] teamMembers = null;
+		int teamLengthRows = names.length / teamMemberLengthCols;;
+		int remainderLength = names.length % teamMemberLengthCols;
+
+		if (remainderLength == 0) { 
+			teamMembers = new String[teamLengthRows][teamMemberLengthCols];
+			System.out.println(teamLengthRows + "개의 팀에 " + teamMemberLengthCols + "명의 팀원을 균등하게 배정합니다.");
+		} else {
+			teamMembers = new String[teamLengthRows][];
+			for (int index = 0; index < remainderLength; index++) {
+				teamMembers[index] = new String[teamMemberLengthCols + 1];
+			}
+
+			for (int index = remainderLength; index < teamLengthRows; index++) {
+				teamMembers[index] = new String[teamMemberLengthCols];
+			}
+
+			/*
+			 			for (int teamRows = 0; teamRows < teamMembers.length; teamRows++) {
+			 				System.out.println((teamRows + 1) + "팀의 팀원은 " + teamMembers[teamRows].length + "명을 배정합니다.");
+			 			}
+			*/
+			System.out.println(teamLengthRows + "개의 팀에 " + teamMemberLengthCols + "명을 배정합니다.");
+			System.out.println("남은 인원은 " + remainderLength + "개의 팀에 1명씩 추가로 균등 배정합니다.");
+		}
+
+		int[] uniqueNamesIndex = new int[names.length];
+		int uniqueLength = 0;
+
+		int randomNo;
+		for (int teamRows = 0; teamRows < teamMembers.length; teamRows++) {
+			for (int teamCols = 0; teamCols < teamMembers[teamRows].length; teamCols++) {
+
+				randomLoop:
+					while(true) {
+						randomNo = getRandomNo();
+						for (int index = 0; index < uniqueLength; index++) {  
+							if (uniqueNamesIndex[index] == randomNo) {
+								continue randomLoop;
+							}
+
+						}
+
+						uniqueNamesIndex[uniqueLength++] = randomNo; 
+						teamMembers[teamRows][teamCols] = names[randomNo];
+						break randomLoop;
+					} 
+
+			}
+		}
+
+		for (int i = 0; i < remainderLength; i++) {    
+			for (int k = 0; k <= teamMemberLengthCols; k++) { 
+				for (int j = 0; j < teamMemberLengthCols; j++) {
+					if ((teamMembers[i][j]).compareTo(teamMembers[i][(j + 1)]) > 0) {
+						String tmp = teamMembers[i][j];
+						teamMembers[i][j] = teamMembers[i][(j + 1)];
+						teamMembers[i][(j + 1)] = tmp;
+					}
+				}
+			}
+		}
+
+		for (int i = remainderLength; i < teamLengthRows; i++) {    
+			for (int k = 0; k <= teamMemberLengthCols; k++) { 
+				for (int j = 0; j < (teamMemberLengthCols - 1); j++) {
+					if ((teamMembers[i][j]).compareTo(teamMembers[i][(j + 1)]) > 0) {
+						String tmp = teamMembers[i][j];
+						teamMembers[i][j] = teamMembers[i][(j + 1)];
+						teamMembers[i][(j + 1)] = tmp;
+					}
+				}
+			}
+		}
+
+		System.out.println();
+		System.out.println("[팀별 팀원 배정 정보]");
+
+		for (int teamRows = 0; teamRows < teamMembers.length; teamRows++) {
+			System.out.print((teamRows + 1) + "팀 팀원 : ");
+			for (int teamCols = 0; teamCols < teamMembers[teamRows].length; teamCols++) {
+				System.out.print(teamMembers[teamRows][teamCols] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static boolean isUniqueNo(int randomNo) {
+		return false;
+	}
+
+	public static int getRandomNo() {
+		Random random = new Random((long)(Math.random() * System.nanoTime()));
+		return random.nextInt(30);
+	}
+}
+
